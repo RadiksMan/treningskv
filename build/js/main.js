42fbb2db2fcc5af3cc82a34fbea265744d166fff
ArtGarm <artem.garmatyk@gmail.com>
2015-12-08 15:49:59 +0200

    dhgn

--- build/js/main.js
+++ build/js/main.js
@@ -5220,6 +5220,56 @@ $(window).resize(function(){
 });
 $(document).ready(function(){
 
+    var today = new Date();
+    today.setDate(today.getDate() + 14);
+    var chistoChislo = today.getDate();
+    var chistoMesec = today.getMonth();
+    var chistoGod = today.getFullYear();
+    var monthRus;
+
+switch (chistoMesec) {
+    case 0:
+        monthRus ='января';
+        break
+    case 1:
+        monthRus ='февраля';
+        break
+    case 2:
+        monthRus ='марта';
+        break
+    case 3:
+        monthRus ='апреля';
+        break
+    case 4:
+        monthRus ='мая';
+        break
+    case 5:
+        monthRus ='июня';
+        break
+    case 6:
+        monthRus ='июля';
+        break
+    case 7:
+        monthRus ='августа';
+        break
+    case 8:
+        monthRus ='сентября';
+        break
+    case 9:
+        monthRus ='октября';
+        break
+    case 10:
+        monthRus ='ноября';
+        break
+    case 11:
+        monthRus ='декабря';
+        break
+}
+
+
+    console.log(monthRus);
+    $('.change-data-fix').text( chistoChislo + ' ' + monthRus + ' ' + chistoGod );
+
 });
 
 $(window).load(function(){
