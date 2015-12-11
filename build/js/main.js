22b5905d881477f10a447afa71770958024f9a37
Radiksman <radion91@gmail.com>
2015-12-11 16:04:36 +0200

    ad some shit

--- build/js/main.js
+++ build/js/main.js
@@ -5271,6 +5271,53 @@ $(document).ready(function(){
     $('.change-data-fix').text( chistoChislo + ' ' + monthRus + ' ' + chistoGod );
     $('.change-data-fix-two').text("до "+chistoChislo + ' ' + monthRus);
 
+        var today_next = new Date();
+        today_next.setDate(today_next.getDate() + 7);
+        var chistoChislo_next = today_next.getDate();
+        var chistoMesec_next = today_next.getMonth();
+        var chistoGod_next = today_next.getFullYear();
+        var monthRus_next;
+        switch (chistoMesec_next) {
+            case 0:
+                monthRus_next ='января';
+                break
+            case 1:
+                monthRus_next ='февраля';
+                break
+            case 2:
+                monthRus_next ='марта';
+                break
+            case 3:
+                monthRus_next ='апреля';
+                break
+            case 4:
+                monthRus_next ='мая';
+                break
+            case 5:
+                monthRus_next ='июня';
+                break
+            case 6:
+                monthRus_next ='июля';
+                break
+            case 7:
+                monthRus_next ='августа';
+                break
+            case 8:
+                monthRus_next ='сентября';
+                break
+            case 9:
+                monthRus_next ='октября';
+                break
+            case 10:
+                monthRus_next ='ноября';
+                break
+            case 11:
+                monthRus_next ='декабря';
+                break
+        }
+
+        $('.hange-data-fix_next').text( chistoChislo_next + ' ' + monthRus_next + ' ' + chistoGod_next );
+
 });
 
 $(window).load(function(){
